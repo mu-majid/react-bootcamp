ec40296df2d6e5139e952504286eac85ce7b97aa
Muhammad Majid <mmajidkamel@gmail.com>
2021-04-03 18:36:56 +0200

    Added files for homepage component

--- 2-clothing-website/src/App.js
+++ 2-clothing-website/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './Homepage.component';
 
 function App() {
   return (
     <div className="App">
-      
+      <Homepage />
     </div>
   );
 }
