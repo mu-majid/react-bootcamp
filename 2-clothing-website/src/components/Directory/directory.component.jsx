cded7019c01b4f2c93872213812325c57ad65788
Muhammad Majid <mmajidkamel@gmail.com>
2021-04-04 15:44:27 +0200

    Added static images for the website, and also refactored sections array into separate file

--- 2-clothing-website/src/components/Directory/directory.component.jsx
+++ 2-clothing-website/src/components/Directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuItem from '../Menu-Item/menu-item.component';
 import './directory.styles.scss';
+import SECTIONS_DATA from "./sections.data.js";
 
 class Directory extends React.Component {
   
@@ -8,40 +9,7 @@ class Directory extends React.Component {
     super(props);
 
     this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
+      sections: SECTIONS_DATA
     }
   }
 
