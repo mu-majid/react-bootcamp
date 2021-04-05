d1619b5d31b95eb261807d0a2028e20ad0f9c639
Muhammad Majid <mmajidkamel@gmail.com>
2021-04-05 17:54:13 +0200

    Storing user (auth on firebase) to our database and update app state with current auth user

--- 2-clothing-website/src/App.js
+++ 2-clothing-website/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Header from './components/Header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Homepage from './pages/Homepage/Homepage.component';
 import ShopPage from './pages/Shop/shop.component';
 import SignInAndUpPage from './pages/Sign-in-up/sign-in-up.component';
@@ -21,10 +21,25 @@ class App extends React.Component {
 
   componentDidMount() {
     // subscribe to firebase auth, we get user session persistence from this subscription
-    this.unsubFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => console.log('Got currentUser snapshot',this.state));
+
+          
+        });
+      }
+      else{
+        // in case of sign out, set currentUser to null
+        this.setState({ currentUser: userAuth });
+      }
 
     });
 
