4bd38e99a92dfb52b786c47aec836c07521af6a9
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-15 15:48:44 +0200

    Cart component now in place

--- 2-clothing-website/src/components/Cart-Icon/cart-icon.component.jsx
+++ 2-clothing-website/src/components/Cart-Icon/cart-icon.component.jsx
@@ -2,3 +2,13 @@ import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
+const CartIcon = () => {
+  return (
+    <div className="cart-icon">
+      <ShoppingIcon className="shopping-icon"/>
+      <span className="item-count">0</span>
+    </div>
+  )
+}
+
+export default CartIcon;
