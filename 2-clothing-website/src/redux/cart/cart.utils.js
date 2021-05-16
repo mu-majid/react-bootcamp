0ba91da6eb4d083e68328dde68258d6518221946
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-16 16:33:05 +0200

    cart grouping of duplicate items

---
+++ 2-clothing-website/src/redux/cart/cart.utils.js
@@ -0,0 +1,11 @@
+export const addCartItem = (cartItems, cartItemToAdd) => {
+  const existingItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+  if (existingItem) {
+    
+    return cartItems.map(cartItem => {
+      return cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : { ...cartItem }
+    });
+  }
+
+  return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ]
+}
