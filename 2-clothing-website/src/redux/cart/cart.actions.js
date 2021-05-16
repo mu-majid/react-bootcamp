2febe3d5a8edd5abaa186438338bec829dca82bd
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-16 15:26:01 +0200

    simple redux config for adding cart item

--- 2-clothing-website/src/redux/cart/cart.actions.js
+++ 2-clothing-website/src/redux/cart/cart.actions.js
@@ -1,3 +1,4 @@
 import { CartActionTypes } from './cart.types';
 
 export const toggleCartHidden = () => ({ type: CartActionTypes.TOGGLE_CART_HIDDEN });
+export const addItem = item => ({type: CartActionTypes.ADD_ITEM, payload: item});
--- 2-clothing-website/src/redux/cart/cart.reducer.js
+++ 2-clothing-website/src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from './cart.types';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -12,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       }
   
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
+      }
     default:
       return state;
   }
