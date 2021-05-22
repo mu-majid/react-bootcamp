0faa70962efda4200117c686eeff4498d78897a1
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-22 13:36:10 +0200

    cart dropdown, handle if cart is empty

--- 2-clothing-website/src/components/Cart-Dropdown/cart-dropdown.component.jsx
+++ 2-clothing-website/src/components/Cart-Dropdown/cart-dropdown.component.jsx
@@ -11,7 +11,10 @@ const CartDropdown = ({ cartItems }) => {
     <div className="cart-dropdown">
       <div className="cart-items">
         {
+          cartItems.length ?
           cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+          :
+          <span className="empty-message">Your Cart Is Empty</span>
         }
       </div>
 
