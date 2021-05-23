d75959fba939ba17b1af8d358279d18f1010f7cb
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-23 20:45:34 +0200

    Collection Page WIP

---
+++ 2-clothing-website/src/pages/Collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => {
+
+  return (
+    <div className="collection-page">
+      Category Page
+    </div>
+  )
+}
+
+export default CollectionPage;
--- 2-clothing-website/src/pages/Shop/shop.component.jsx
+++ 2-clothing-website/src/pages/Shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/Collections-Overview/collections-overview.component';
+import CollectionPage from '../Collection/collection.component';
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route path={`${match.path}`} component={CollectionsOverview} />
+      <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   )
 }
