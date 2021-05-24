0aeda51a4a77ee9712c76f5cabc4705e126d85eb
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-24 19:07:16 +0200

    connecting collection page to store with selectors

--- 2-clothing-website/src/pages/Collection/collection.component.jsx
+++ 2-clothing-website/src/pages/Collection/collection.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
@@ -10,4 +12,8 @@ const CollectionPage = ({ match }) => {
   )
 }
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- 2-clothing-website/src/redux/shop/shop.selectors.js
+++ 2-clothing-website/src/redux/shop/shop.selectors.js
@@ -1,5 +1,13 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_URL_ID_MAP = {
+  hats: 1,
+  sneaker: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+}
+
 // Memoization of a redux state selector to prevent multiple un-necessary renders
 
 const selectShop = state => state.shop;
@@ -7,3 +15,8 @@ export const selectShopCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+export const selectCollection = collectionUrlParam => createSelector(
+  [selectShopCollections],
+  (collections) => collections.find(c => c.id === COLLECTION_URL_ID_MAP[collectionUrlParam])
+)
