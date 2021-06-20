d61181716dc27b8f3ab286faa2875d80ed525e57
Muhammad Majid <mmajidkamel@gmail.com>
2021-06-20 13:06:21 +0200

    added saga file for shop component

---
+++ 2-clothing-website/src/redux/shop/shop.sagas.js
@@ -0,0 +1,26 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop.actions';
+import shopActionsTypes from './shop.types';
+
+
+export function* fetchCollectionsAsync() {
+  yield console.log('I am fired');
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionMap));
+  }
+  catch (error) {
+    yield put(fetchCollectionsFailure(collectionMap));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  takeEvery(
+    shopActionsTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  )
+}
