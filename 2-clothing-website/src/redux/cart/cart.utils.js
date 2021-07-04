5a180281384e62b65a48c4bd93bf38a3c3c7a9aa
mu-majid <mmajidkamel@gmail.com>
2021-07-04 22:00:04 +0200

    convert cart into saga, and clear it out after sign out

--- 2-clothing-website/src/redux/cart/cart.utils.js
+++ 2-clothing-website/src/redux/cart/cart.utils.js
@@ -1,23 +1,31 @@
-export const addCartItem = (cartItems, cartItemToAdd) => {
-  const existingItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-  if (existingItem) {
-    
-    return cartItems.map(cartItem => {
-      return cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : { ...cartItem }
-    });
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
   }
 
-  return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ]
-}
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
 
-export const deleteCartItem = (cartItems, cartItemToDelete) => cartItems.filter(item => item.id !== cartItemToDelete.id);
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
 
-export const removeSingleCartItem = (cartItems, singleItemToRemove) => {
-  const existingItem = cartItems.find(cartItem => cartItem.id === singleItemToRemove.id);
-
-  if (existingItem.quantity === 1) {
-    return deleteCartItem(cartItems, existingItem);
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
   }
 
-  return cartItems.map(item => (item.id === singleItemToRemove.id ? { ...item, quantity: item.quantity - 1 } : item))
-}
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
