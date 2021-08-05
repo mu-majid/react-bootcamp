79edd0bcf7580d3dcb5aa9e1f7142876fec914d4
mu-majid <mmajidkamel@gmail.com>
2021-08-05 20:42:55 +0200

    Added some testing for some components

--- 2-clothing-website/client/src/components/Collection-Item/collection-item.styles.jsx
+++ 2-clothing-website/client/src/components/Collection-Item/collection-item.styles.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Button from '../Button/button.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 export const CollectionItemContainer = styled.div`
   width: 22vw;
@@ -17,15 +17,34 @@ export const CollectionItemContainer = styled.div`
       display: flex;
     }
   }
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+      button {
+        opacity: unset;
+      }
+    }
+  }
 `;
 
-export const AddButton = styled(Button)`
+export const AddButton = styled(CustomButton)`
   width: 80%;
   opacity: 0.7;
   position: absolute;
   top: 255px;
   display: none;
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    padding: 0 10px;
+  }
 `;
+
+AddButton.displayName = 'AddButton';
 
 export const BackgroundImage = styled.div`
   width: 100%;
@@ -36,6 +55,8 @@ export const BackgroundImage = styled.div`
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
 
+BackgroundImage.displayName = 'BackgroundImage';
+
 export const CollectionFooterContainer = styled.div`
   width: 100%;
   height: 5%;
@@ -44,12 +65,18 @@ export const CollectionFooterContainer = styled.div`
   font-size: 18px;
 `;
 
+CollectionFooterContainer.displayName = 'CollectionFooterContainer';
+
 export const NameContainer = styled.span`
   width: 90%;
   margin-bottom: 15px;
 `;
 
+NameContainer.displayName = 'NameContainer';
+
 export const PriceContainer = styled.span`
   width: 10%;
   text-align: right;
 `;
+
+PriceContainer.displayName = 'PriceContainer';
